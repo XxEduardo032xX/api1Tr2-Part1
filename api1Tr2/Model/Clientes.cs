102a49743d071ddb53163c4afd241fcc321fa164
XxEduardo032xX <153869902+XxEduardo032xX@users.noreply.github.com>
2024-03-25 23:54:36 -0500

    Modificando string de los campos

--- api1Tr2/Model/Clientes.cs
+++ api1Tr2/Model/Clientes.cs
@@ -3,12 +3,12 @@
     public class Clientes
     {
         public int ID_Cliente { get; set; }
-        public String Nombre { get; set; }
-        public String Apellido { get; set; }
-        public String CorreoElectronico { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoElectronico { get; set; }
         public int Telefono { get; set; }
-        public String Direccion { get; set; }
-        public String OtrosDetalles { get; set; }
+        public string Direccion { get; set; }
+        public string OtrosDetalles { get; set; }
 
     }
 
